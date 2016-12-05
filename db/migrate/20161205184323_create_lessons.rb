a82a2febb488584b4f8be965542b6ccd361a4fc4
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-12-05 13:56:53 -0500

    Delete user class, create student teacher class

--- db/migrate/20161205184323_create_lessons.rb
+++ db/migrate/20161205184323_create_lessons.rb
@@ -2,6 +2,8 @@ class CreateLessons < ActiveRecord::Migration[5.0]
   def change
     create_table :lessons do |t|
 
+      t.datetime "time"
+      t.integer "user_id"
       t.timestamps
     end
   end
