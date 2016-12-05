5e5dc0baf6e54839408b14e08bdeb56130f565f9
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-12-05 18:34:49 -0500

    Create student from logged in user

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,18 @@
 class StudentsController < ApplicationController
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(:user_id => current_user.id)
+
+    if @student.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+
+  end
+
 end
