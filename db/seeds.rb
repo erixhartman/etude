53ed1b9d7b03a9b9933ba834ec0a8df2f4d6bc1b
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-12-05 14:59:53 -0500

    Add 4 seeded teachers and 1 seeded student

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Teacher.create(
+  first_name: "Eric",
+  last_name: "Hartman",
+  email: "eah458@nyu.edu",
+  price: 20,
+  subject: 'guitar',
+)
+
+Teacher.create(
+  first_name: "Luke",
+  last_name: "Plourde",
+  email: "luke@luke.com",
+  price: 40,
+  subject: 'guitar',
+)
+
+Teacher.create(
+  first_name: "Ian",
+  last_name: "Russell",
+  email: "ian@ian.com",
+  price: 40,
+  subject: 'spanish',
+)
+
+Teacher.create(
+  first_name: "Mike",
+  last_name: "Schwartze",
+  email: "mike@mike.com",
+  price: 20,
+  subject: 'spanish',
+)
+
+Student.create(
+  first_name: "Seat",
+  last_name: "Yourself",
+  email: "seat@yourself.com"
+)
