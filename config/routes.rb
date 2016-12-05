7e3a9507525a032d20ffc40d10afd188773d930b
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-12-05 14:38:52 -0500

    Add routes for Student, Teacher, and Lesson

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  resources :students, only: [:new, :create] do
+    resources :lessons, only: [:index, :show]
+  end
+
+  resources :teachers, only: [:new, :create, :index, :show] do
+    resources :lessons, only: [:index, :show]
+  end
+
+  resources :lessons, only: [:new, :create]
 end
