0234c6d8eb694590110fd1cc271142deeacaeb6d
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-12-05 14:06:37 -0500

    Create attr for student, teacher and lesson

--- db/migrate/20161205184323_create_lessons.rb
+++ db/migrate/20161205184323_create_lessons.rb
@@ -1,9 +1,9 @@
 class CreateLessons < ActiveRecord::Migration[5.0]
   def change
     create_table :lessons do |t|
-
       t.datetime "time"
-      t.integer "user_id"
+      t.integer "student_id"
+      t.integer "teacher_id"
       t.timestamps
     end
   end
--- db/migrate/20161205185555_create_teachers.rb
+++ db/migrate/20161205185555_create_teachers.rb
@@ -1,7 +1,12 @@
 class CreateTeachers < ActiveRecord::Migration[5.0]
   def change
     create_table :teachers do |t|
-
+      t.string "first_name"
+      t.string "last_name"
+      t.string "email"
+      t.integer "price"
+      t.string "subject"
+      t.string "postal_code"
       t.timestamps
     end
   end
