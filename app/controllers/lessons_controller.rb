3983c391de48396ef839e03dc365eda7c68c42f5
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-12-06 14:14:48 -0500

    Add Book Lesson link

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,3 +1,29 @@
 class LessonsController < ApplicationController
-  
+
+  def new
+    @lesson = Lessson.new
+  end
+
+  def create
+    @user = current_user
+    @student = @user.student.id
+    @lesson = @teacher.Lesson.new(
+      teacher_id: @teacher.id,
+      time: lesson_params[:time],
+      student_id: @student
+    )
+    if @lesson.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def lesson_params
+    params.require(:lesson).permit(:time)
+  end
+
+
 end
