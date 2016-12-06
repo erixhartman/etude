583edc566aa11b5dc98c4513f70fba3303a85277
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-12-05 19:04:49 -0500

    New teacher form NOT DONE

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -3,4 +3,26 @@ class TeachersController < ApplicationController
     @teachers = Teacher.all
   end
 
+  def new
+    @teacher = Teacher.new
+  end
+
+  def create
+    @teacher = Teacher.new(teacher_params)
+
+    if @teacher.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+
+  end
+
+  private
+
+  def teacher_params
+    user_id = current_user.id
+    params.require(:teacher).permit(:price, :subject, :postal_code, :user_id )
+  end
+
 end
