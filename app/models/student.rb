f7e5802b9f274bad5d3e335ba3943096b82cd2b7
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-12-06 15:30:58 -0500

    Add methods for first_name, last_name, email

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,16 @@ class Student < ApplicationRecord
   belongs_to :user
   has_many :lessons
   has_many :teachers, through: :lessons
+
+  def first_name
+    User.find(self.user_id).first_name
+  end
+
+  def last_name
+    User.find(self.user_id).last_name
+  end
+
+  def email
+    User.find(self.user_id).email
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   root :to => redirect('/teachers')
-  resources :students, only: [:new, :create] do
-    resources :lessons, only: [:index, :show]
-  end
+  resources :students, only: [:new, :create]
 
   resources :teachers, only: [:new, :create, :index, :show] do
-    resources :lessons, only: [:index, :show, :new, :create]
+    resources :lessons, only: [:new, :create]
   end
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
 
   resources :sessions, only: [:new, :create, :destroy]
 end
