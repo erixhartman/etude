bd698b0b2fd2bda65616835ebbecabe025bcc37e
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-12-07 13:40:02 -0500

    email must be vaild

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_one :student
   has_one :teacher
   has_secure_password
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
