587576447e43a781fea5140a74c182c7b7f20095
Eric Hartman <imerichartman@gmail.com>
2016-12-08 18:30:23 -0500

    Check validation for teacher general availability

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,12 +5,22 @@ class Lesson < ApplicationRecord
   validates :time, allow_blank: false, presence: true
   validate :lesson_active, on: :create
   validate :teacher_must_be_available, on: :create
+  validate :teacher_must_be_generally_available, on: :create
 
   def teacher_must_be_available
     unless teacher.available?(time)
       errors.add(:time, "Teacher is not available at this time")
     end
   end
+
+
+  def teacher_must_be_generally_available
+    unless teacher.availability_ok?(time)
+      errors.add(:time, "Teacher does not teach during these hours")
+    end
+  end
+
+
 
   def lesson_active
     if self.time != nil
