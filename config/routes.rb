09c7ebc3ae584541202a2d14fa8b3cacd931117a
Eric Hartman <imerichartman@gmail.com>
2016-12-08 16:17:55 -0500

    Fix route error for availability range

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
 
-  resources :availability_ranges, only [:new, :create, :edit, :update]
+  resources :availability_ranges, only: [:new, :create, :edit, :update]
 
 end
