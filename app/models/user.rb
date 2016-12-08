1e7a9411a0ca395a2ab4376ab734a272a07c88be
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-12-08 13:12:42 -0500

    Full Error mmessage's added for sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, :password, :password_confirmation, presence: true
   validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
 end
