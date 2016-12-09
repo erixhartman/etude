1318e26bb426c41e416fd2caff7f98092fa3713a
Eric Hartman <imerichartman@gmail.com>
2016-12-09 18:29:59 -0500

    Change migration to change student table

--- db/migrate/20161205204250_delete_user_params_from_student.rb
+++ db/migrate/20161205204250_delete_user_params_from_student.rb
@@ -1,5 +1,7 @@
 class DeleteUserParamsFromStudent < ActiveRecord::Migration[5.0]
-  def change_table(:students) do |t|
-      t.remove :first_name, :last_name, :email
+  def change
+    def change_table(:students) do |t|
+        t.remove :first_name, :last_name, :email
+    end
   end
 end
