6299d1d105a3cccc7d5d110352b4cd4e7574a231
Eric Hartman <imerichartman@gmail.com>
2016-12-09 18:41:17 -0500

    Attempt to fix heroku migration by eliminating bad migrations

--- db/migrate/20161205204256_delete_user_params_from_teacher.rb
+++
@@ -1,7 +0,0 @@
-class DeleteUserParamsFromTeacher < ActiveRecord::Migration[5.0]
-  def change
-    def change_table(:teachers) do |t|
-        t.remove :first_name, :last_name, :email
-    end
-  end
-end
