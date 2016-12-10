e532262bd3b8ce142e406f7826de4b3c65a7bc72
Ian Russell <ian.andrew.russell@gmail.com>
2016-12-10 11:22:13 -0500

    Fix password can't be blank error appearing twice by removing password from validates field. has_secure_password automatically checks for its presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_one :teacher
   has_secure_password
 
-  validates :first_name, :last_name, :password, :password_confirmation, presence: true
+  validates :first_name, :last_name, :password_confirmation, presence: true
   validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 
 end
