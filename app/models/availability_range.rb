ae75a87585f5a3a68d7b528fc2b8c9c85a6cc8ff
Michael Schwartze <mschwartze@gmail.com>
2016-12-11 16:09:32 -0500

    AM PM fix for HOURS_OF_DAY const

--- app/models/availability_range.rb
+++ app/models/availability_range.rb
@@ -33,7 +33,7 @@ class AvailabilityRange < ApplicationRecord
   end
 
   HOURS_OF_DAY =
-  {"12 AM" => 0, 
+  {"12 AM" => 0,
     "1 AM" => 1,
     "2 AM" => 2,
     "3 AM" => 3,
@@ -41,12 +41,12 @@ class AvailabilityRange < ApplicationRecord
     "5 AM" => 5,
     "6 AM" => 6,
     "7 AM" => 7,
-    "8 PM" => 8,
+    "8 AM" => 8,
     "9 AM" => 9,
     "10 AM" => 10,
     "11 AM" => 11,
     "12 PM" => 12,
-    "1 PM" => 13 ,
+    "1 PM" => 13,
     "2 PM" => 14,
     "3 PM" => 15,
     "4 PM" => 16,
