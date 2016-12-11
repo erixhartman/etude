0ad2ec5a05434a3ed772ee5eafaf795d690024f6
Ian Russell <ian.andrew.russell@gmail.com>
2016-12-11 15:47:14 -0500

    Create constant in availability model for to assign integer values a time

--- app/models/availability_range.rb
+++ app/models/availability_range.rb
@@ -32,4 +32,31 @@ class AvailabilityRange < ApplicationRecord
     end
   end
 
+  HOURS_OF_DAY =
+  {"12 AM" => 0, 
+    "1 AM" => 1,
+    "2 AM" => 2,
+    "3 AM" => 3,
+    "4 AM" => 4,
+    "5 AM" => 5,
+    "6 AM" => 6,
+    "7 AM" => 7,
+    "8 PM" => 8,
+    "9 AM" => 9,
+    "10 AM" => 10,
+    "11 AM" => 11,
+    "12 PM" => 12,
+    "1 PM" => 13 ,
+    "2 PM" => 14,
+    "3 PM" => 15,
+    "4 PM" => 16,
+    "5 PM" => 17,
+    "6 PM" => 18,
+    "7 PM" => 19,
+    "8 PM" => 20,
+    "9 PM" => 21,
+    "10 PM" => 22,
+    "11 PM" => 23
+  }
+
 end
