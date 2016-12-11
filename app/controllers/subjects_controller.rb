cdc2d9f04e453340f204b97f09915ef0129c066a
Michael Schwartze <mschwartze@gmail.com>
2016-12-11 12:15:18 -0500

    Add teachers variable that finds teachers by the subjects param

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,7 +1,14 @@
 class SubjectsController < ApplicationController
   def index
+    @teachers = Teacher.where(subject: subject_params)
   end
 
   def show
   end
+
+  private
+  def subject_params
+    params.require(:subject)
+  end
+
 end
