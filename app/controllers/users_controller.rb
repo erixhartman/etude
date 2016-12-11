d065dc720bffe6db192046e8fa9fbf9e51f759ea
Michael Schwartze <mschwartze@gmail.com>
2016-12-11 14:16:25 -0500

    Change find to find_by in show controller method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     if @user.teacher
-      @teacher = Teacher.find(@user.id)
+      @teacher = Teacher.find_by(user_id: @user.id)
     end
   end
 
