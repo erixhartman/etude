d57a515a4972f2b3db2707fcb364a29cf318bf2a
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-12-14 14:59:12 -0500

    Add address to student

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209184717) do
+ActiveRecord::Schema.define(version: 20161214193127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,9 +33,12 @@ ActiveRecord::Schema.define(version: 20161209184717) do
   end
 
   create_table "students", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
     t.integer  "user_id"
+    t.string   "street_address"
+    t.string   "city"
+    t.string   "postal_code"
   end
 
   create_table "teachers", force: :cascade do |t|
